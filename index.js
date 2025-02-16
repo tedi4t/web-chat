7fc667074660c0f9613fc652ee2db0392f907599
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2025-02-16 20:44:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ io.on("connection", (socket) => {
     io.emit("chat message", msg); // Broadcast message
   });
 
+
+
+
+
+  
+
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
