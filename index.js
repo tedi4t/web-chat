aa26d284fa55457380fb38e232025f90abe137ff
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2025-02-17 22:48:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 
+
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
